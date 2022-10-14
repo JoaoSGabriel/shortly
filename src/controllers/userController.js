564c659ec410a0fe18d6183ee8d2e9af69f7b2b0
JoaoSGabriel <joaogabriel0359@gmail.com>
2022-10-14 10:56:08 -0300

    test route to get user Urls

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,9 +3,14 @@ import connection from "../db.js";
 export async function getUserUrls(req, res) {
   try {
     const promisse = await connection.query(
-      `SELECT users.*, SUM(urls."visitCount") as "visitCount" FROM users JOIN urls ON urls."userId" = users.id WHERE users.id = $1;`,
+      `SELECT users.*, SUM(urls."visitCount") as "visitCount" FROM users JOIN urls ON urls."userId" = users.id WHERE users.id = $1 GROUP BY users.id;`,
       [res.locals.user]
     );
+
+    if (!promisse.rows[0]) {
+      res.sendStatus(404);
+      return;
+    }
 
     const shortenUrls = await connection.query(
       `SELECT id, "shortUrl", url, "visitCount" FROM urls WHERE "userId" = $1;`,
@@ -29,7 +34,7 @@ export async function getUserUrls(req, res) {
 export async function getRanking(req, res) {
   try {
     const promisse = await connection.query(
-      `SELECT users.id, users.name, COUNT(urls."userId") as "linksCount", SUM(urls."visitCount") as "visitCount" FROM urls LEFT JOIN users ON shortenedurls "userId" = users.id GROUP BY users.id ORDER BY "visitCount" ASC LIMIT 10;`
+      `SELECT users.id, users.name, COUNT(urls."userId") as "linksCount", SUM(urls."visitCount") as "visitCount" FROM urls FULL JOIN users ON urls."userId" = users.id GROUP BY users.id ORDER BY "visitCount" DESC LIMIT 10;`
     );
 
     res.status(200).send(promisse.rows);
